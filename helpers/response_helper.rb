4b4dc598e8e5eaa78823e610be91193e5103f57b
David Shorthouse <davidpshorthouse@gmail.com>
2024-02-28 00:41:27 -0500

    Send etag with images

--- helpers/response_helper.rb
+++ helpers/response_helper.rb
@@ -6,12 +6,14 @@ module Sinatra
  
        def nil_image
          content_type "image/png"
+         etag "1x1"
          send_file "system/1x1.png", type: :png
          exit
        end
 
        def get_image(hex:)
          content_type "image/png"
+         etag hex
          send_file "public/#{hex}.png", type: :png
        end
 
@@ -36,6 +38,7 @@ module Sinatra
                image.resize!(:auto, params[:height].to_i)
             end
             image.save("public/#{hex}.png")
+            etag hex
             send_file "public/#{hex}.png", type: :png
          rescue Rszr::LoadError => e
             redirect uri.to_s
