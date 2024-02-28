01d86ad486aa1bbfddd742c148a2d1391cab7055
David Shorthouse <davidpshorthouse@gmail.com>
2024-02-28 01:34:45 -0500

    Add delete routes to flush cached files

--- routes/image_route.rb
+++ routes/image_route.rb
@@ -18,6 +18,27 @@ module Sinatra
                   save_image(params: params, hex: hex)
                end
             end
+
+            app.delete '/etag/:hex' do
+               if params[:hex] && 
+                  params[:hex].length == 32 && 
+                  File.exist?(File.join("public", "#{hex}.png"))
+                     flush_image(hex: params[:hex])
+                     halt 410, "File is gone"
+               else
+                  halt 404, "File not found"
+               end
+            end
+
+            app.delete '/etags' do
+               params[:etags].split("\r\n").each do |hex|
+                  if hex.length == 32 && 
+                     File.exist?(File.join("public", "#{hex}.png"))
+                        flush_image(hex: hex)
+                  end
+               end
+               halt 410, "Files are gone"
+            end
          end
 
       end
